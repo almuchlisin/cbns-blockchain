b96df79a0eb2bd3e20812c99d1df49963a771100
almuchlisin <80834158+almuchlisin@users.noreply.github.com>
2024-09-11 12:27:01 +0700

    Update pbft-node.h

--- model/pbft-node.h
+++ model/pbft-node.h
@@ -1,5 +1,5 @@
-#ifndef PBFT_NODE_H
-#define PBFT_NODE_H
+#ifndef POA_NODE_H
+#define POA_NODE_H
 
 #include <algorithm>
 #include "ns3/application.h"
@@ -9,91 +9,84 @@
 #include "ns3/address.h"
 #include "ns3/boolean.h"
 #include <map>
- 
+
 namespace ns3 {
 
 class Address;
 class Socket;
 class Packet;
- 
-class PbftNode : public Application 
+
+class PoaNode : public Application 
 {
-  public:
+public:
     static TypeId GetTypeId (void);
 
-    void SetPeersAddresses (const std::vector<Ipv4Address> &peers);         // 设置所有邻节点的地址
+    void SetPeersAddresses (const std::vector<Ipv4Address> &peers);         // Set the addresses of all neighbor nodes
 
-    PbftNode (void);
+    PoaNode (void);
+    virtual ~PoaNode (void);
 
-    virtual ~PbftNode (void);
+    // Node information
+    uint32_t        m_id;                               // Node ID
+    Ptr<Socket>     m_socket;                           // Listening socket
+    Ptr<Socket>     m_socketClient;                     // Client socket
+    std::map<Ipv4Address, Ptr<Socket>>      m_peersSockets; // List of neighbor node sockets
+    std::map<Address, std::string>          m_bufferedData; // Map holding the buffered data from previous handleRead events
+    Address         m_local;                            // Local address
+    std::vector<Ipv4Address>  m_peersAddresses;         // List of neighbor nodes
 
-    uint32_t        m_id;                               // node id
-    Ptr<Socket>     m_socket;                           // 监听的socket
-    Ptr<Socket>     m_socketClient;                     // 客户端socket
-    std::map<Ipv4Address, Ptr<Socket>>      m_peersSockets;            // 邻节点的socket列表
-    std::map<Address, std::string>          m_bufferedData;            // map holding the buffered data from previous handleRead events
-    
-    Address         m_local;                            // 本节点地址
-    std::vector<Ipv4Address>  m_peersAddresses;         // 邻节点列表
+    int             N;                                  // Total number of nodes
+    bool            is_validator;                       // Is this node a validator (Proof of Authority)
+    int             m_value;                            // Value modified through consensus
+    char            proposal;                           // Command to be sent
+    int             blockNum;                           // Number of blocks processed
+    int             round;                              // Consensus round
+    float           heartbeat_timeout;                  // Heartbeat timeout interval
+    int             add_change_value;                   // Whether to include proposal in heartbeat
+    EventId         m_nextBlock;                        // Next block creation event (specific to PoA)
 
-    int             N;                                  // 总节点数
-    // int             v;                                  // 当前视图编号
-    // int             n;                                  // 当前消息在视图中的编号
-    std::vector<int>  values;                           // 存储要更新的数值的容器
-    int             value;                              // 交易要更新的值
-    int             leader;                             // 当前leader节点的编号
-    int             is_leader;                          // 自己是否是leader
-
-    // int             round;                              // 共识轮数
-    int             block_num;                             // 当前区块号
-    EventId         blockEvent;                         // 广播区块的事件
-    struct TX {
-        int v;
-        int val;
-        int prepare_vote;
-        int commit_vote;
-    };
-    TX tx[1000];
-
-    // 继承 Application 类必须实现的虚函数
+    // Inherited Application class methods
     virtual void StartApplication (void);    
     virtual void StopApplication (void); 
 
-    // 处理消息
+    // Message handling methods
     void HandleRead (Ptr<Socket> socket);
-
-    // 将数据包中的消息解析为字符串
-    std::string getPacketContent(Ptr<Packet> packet, Address from); 
-
-    // 向所有邻节点广播消息 
     void Send (uint8_t data[]);
+    void Send (uint8_t data[], Address from);
+    void SendTX (uint8_t data[], int num);
+    void SendBlock (uint8_t data[]);                     // PoA specific: Send block
 
-    // 向某个指定地址的节点发送消息
-    void Send(uint8_t data[], Address from);
+    // Proposal setting method
+    void setProposal (void);
 
-    // 向所有邻节点广播区块
-    void SendBlock(void);
-
-    void viewChange(void);
+    // PoA Specific Methods
+    void DetermineValidator (void);                      // PoA specific: Determine if node is a validator
+    void CreateBlock (void);                             // PoA specific: Create block
+    std::string getPacketContent (Ptr<Packet> packet, Address from); 
 };
 
 enum Message
 {
-    REQUEST,           // 0       客户端请求        <REQUEST, t>    t:交易
-    PRE_PREPARE,       // 1       预准备消息        <PRE_PREPARE, v, n, b>   v:视图编号   b:区块内容   n:该预准备消息在视图中的编号
-    PREPARE,           // 2       准备消息          <PREPARE, v, n, H(b)>
-    COMMIT,            // 3       提交             <COMMIT, v, n>
-    PRE_PREPARE_RES,   // 4       预准备消息的响应   <PRE_PREPARE_RES, v, n, S>           S:State
-    PREPARE_RES,       // 5       准备消息响应
-    COMMIT_RES,        // 6       提交响应
-    REPLY,             // 7       对客户端的回复       
-    VIEW_CHANGE        // 8       view_change消息
+    CLIENT_REQ,        // 0       Client request
+    CLIENT_RES,        // 1       Response to client
+    VOTE_REQ,          // 2       Vote request (Not used in PoA)
+    VOTE_RES,          // 3       Vote response (Not used in PoA)
+    HEARTBEAT,         // 4       Heartbeat
+    HEARTBEAT_RES,     // 5       Heartbeat response
+};
+
+enum HeartBeatType
+{
+    HEART_BEAT,       // 0        Regular heartbeat
+    PROPOSAL,         // 1        Heartbeat with proposal
 };
 
 enum State
 {
-    SUCCESSED,            // 0      成功
-    FAILED,             // 1      失败
+    POA_SUCCESS,      // 0      Success
+    POA_FAILED,       // 1      Failed
 };
-}
-#endif
+
+} // namespace ns3
+
+#endif // POA_NODE_H
