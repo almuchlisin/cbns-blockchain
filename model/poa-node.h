4aed6140030b5cc359b2c1353c98ae85d82f1f99
almuchlisin <80834158+almuchlisin@users.noreply.github.com>
2024-09-11 12:41:13 +0700

    Update poa-node.h

--- model/poa-node.h
+++ model/poa-node.h
@@ -1,6 +1,7 @@
 #ifndef POA_NODE_H
 #define POA_NODE_H
 
+#include <algorithm>
 #include "ns3/application.h"
 #include "ns3/event-id.h"
 #include "ns3/ptr.h"
@@ -17,46 +18,75 @@ class Packet;
 
 class PoaNode : public Application 
 {
-  public:
+public:
     static TypeId GetTypeId (void);
 
-    void SetPeersAddresses (const std::vector<Ipv4Address> &peers); 
+    void SetPeersAddresses (const std::vector<Ipv4Address> &peers);         // Set the addresses of all neighbor nodes
 
     PoaNode (void);
     virtual ~PoaNode (void);
 
-    uint32_t m_id;
-    Ptr<Socket> m_socket;
-    Ptr<Socket> m_socketClient;
-    std::map<Ipv4Address, Ptr<Socket>> m_peersSockets;
-    std::map<Address, std::string> m_bufferedData;
-    
-    Address m_local;
-    std::vector<Ipv4Address> m_peersAddresses;
+    // Node information
+    uint32_t        m_id;                               // Node ID
+    Ptr<Socket>     m_socket;                           // Listening socket
+    Ptr<Socket>     m_socketClient;                     // Client socket
+    std::map<Ipv4Address, Ptr<Socket>>      m_peersSockets; // List of neighbor node sockets
+    std::map<Address, std::string>          m_bufferedData; // Map holding the buffered data from previous handleRead events
+    Address         m_local;                            // Local address
+    std::vector<Ipv4Address>  m_peersAddresses;         // List of neighbor nodes
 
-    int N;
-    bool is_validator;
-    int m_value;
-    char proposal;
-    int blockNum;
-    int round;
-    uint8_t* tx;
+    int             N;                                  // Total number of nodes
+    bool            is_validator;                       // Is this node a validator (Proof of Authority)
+    int             m_value;                            // Value modified through consensus
+    char            proposal;                           // Command to be sent
+    int             blockNum;                           // Number of blocks processed
+    int             round;                              // Consensus round
+    float           heartbeat_timeout;                  // Heartbeat timeout interval
+    int             add_change_value;                   // Whether to include proposal in heartbeat
+    EventId         m_nextBlock;                        // Next block creation event (specific to PoA)
 
-    // Deklarasi variabel yang sebelumnya tidak ditemukan
-    float heartbeat_timeout;
-    int add_change_value;
-    EventId m_nextBlock;
-
+    // Inherited Application class methods
     virtual void StartApplication (void);    
     virtual void StopApplication (void); 
 
+    // Message handling methods
     void HandleRead (Ptr<Socket> socket);
-    void SendBlock(uint8_t data[]);
-    std::string getPacketContent(Ptr<Packet> packet, Address from); 
-    void CreateBlock();
-    void DetermineValidator(); // Tambahkan deklarasi fungsi ini
+    void Send (uint8_t data[]);
+    void Send (uint8_t data[], Address from);
+    void SendTX (uint8_t data[], int num);
+    void SendBlock (uint8_t data[]);                     // PoA specific: Send block
+
+    // Proposal setting method
+    void setProposal (void);
+
+    // PoA Specific Methods
+    void DetermineValidator (void);                      // PoA specific: Determine if node is a validator
+    void CreateBlock (void);                             // PoA specific: Create block
+    std::string getPacketContent (Ptr<Packet> packet, Address from); 
+};
+
+enum Message
+{
+    CLIENT_REQ,        // 0       Client request
+    CLIENT_RES,        // 1       Response to client
+    VOTE_REQ,          // 2       Vote request (Not used in PoA)
+    VOTE_RES,          // 3       Vote response (Not used in PoA)
+    HEARTBEAT,         // 4       Heartbeat
+    HEARTBEAT_RES,     // 5       Heartbeat response
+};
+
+enum HeartBeatType
+{
+    HEART_BEAT,       // 0        Regular heartbeat
+    PROPOSAL,         // 1        Heartbeat with proposal
+};
+
+enum State
+{
+    POA_SUCCESS,      // 0      Success
+    POA_FAILED,       // 1      Failed
 };
 
 } // namespace ns3
 
-#endif /* POA_NODE_H */
+#endif // POA_NODE_H
